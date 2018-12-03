b74edb32275659e228e8ee7a8f45b3f21311ea0c
LiuXin <32125402+GeorgeLiuXin@users.noreply.github.com>
2018-12-03 21:08:51 +0800

    GMCommand
    GM指令添加

---
+++ Assets/Scripts/GMCommand/GMCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XWorld
+{
+    public class GMCommand : Singleton<GMCommand>
+    {
+        public delegate void GMCommandFunction(params object[] values);
+
+        public static Dictionary<string, EventsDelegate.EventFunction> m_GMCommandDict;
+
+        public GMCommand()
+        {
+            m_GMCommandDict = new Dictionary<string, EventsDelegate.EventFunction>();
+            InitGM();
+        }
+
+        public void InitGM()
+        {
+
+        }
+
+    }
+}
