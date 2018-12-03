d5fc5d0475078e9568ffeb787003d565746d2a53
LiuXin <32125402+GeorgeLiuXin@users.noreply.github.com>
2018-12-04 00:45:50 +0800

    GMCommand
    添加GM指令系统，方便之后后端开发效率

--- Assets/Scripts/GMCommand/GMCommand.cs
+++ Assets/Scripts/GMCommand/GMCommand.cs
@@ -5,22 +5,54 @@ using UnityEngine;
 
 namespace XWorld
 {
-    public class GMCommand : Singleton<GMCommand>
+    public partial class GMCommand : Singleton<GMCommand>
     {
         public delegate void GMCommandFunction(params object[] values);
 
-        public static Dictionary<string, EventsDelegate.EventFunction> m_GMCommandDict;
+        public static Dictionary<string, GMCommandFunction> m_GMCommandDict;
 
         public GMCommand()
         {
-            m_GMCommandDict = new Dictionary<string, EventsDelegate.EventFunction>();
+            m_GMCommandDict = new Dictionary<string, GMCommandFunction>();
             InitGM();
         }
 
         public void InitGM()
         {
+			if (m_GMCommandDict == null)
+				return;
 
+			AddGMCommand("test", GMCommand_Test);
         }
+
+		private void AddGMCommand(string gm, GMCommandFunction func)
+		{
+			if (m_GMCommandDict.ContainsKey(gm))
+			{
+				GameLogger.Error(LOG_CHANNEL.LOGIC, "GMCommand Add Error!");
+				return;
+			}
+
+			m_GMCommandDict.Add(gm, func);
+		}
+
+		public void HandleGMCommand(string command)
+		{
+			string[] strs = command.Split(' ');
+			if (strs.Length <= 0)
+				return;
+
+			string key = strs[0];
+			if (!m_GMCommandDict.ContainsKey(key))
+			{
+				GameLogger.Error(LOG_CHANNEL.LOGIC, "GMCommand don't contain this command!");
+				return;
+			}
+			List<string> list = new List<string>(strs);
+			list.RemoveAt(0);
+			object[] objs = list.ToArray();
+			m_GMCommandDict[key](objs);
+		}
 
     }
 }
