ff0bd36d75c0fe9a4dfd9d568510e9dbd45da29f
LiuXin <32125402+GeorgeLiuXin@users.noreply.github.com>
2018-12-04 20:51:34 +0800

    ResourcesLoader
    资源加载 仅第一版demo时可用，当前用Resources来加载资源

--- Assets/Scripts/AssetPipeline/Manager/ResourceLoader.cs
+++ Assets/Scripts/AssetPipeline/Manager/ResourceLoader.cs
@@ -33,7 +33,23 @@ namespace XWorld.AssetPipeline
 
         public abstract void Init();
 
+        public abstract byte[] LoadByteBuffer(string mainPath);
+        public abstract byte[] LoadFileInStreamingAssets(string fileName);
+        public abstract void LoadImagePack(string relativePath, LoadAssetCallback callback, LoadAssetCallback singleCallback, object userdata = null);
         public abstract int LoadAsset(string rc, LoadAssetCallback callback, object userdata = null);
+        public abstract int LoadAsset(string rc, int priority, LoadAssetCallback callback, object userdata = null);
+        public abstract int[] LoadAssets(string[] rc, LoadAssetCallback callback, LoadAssetCallback singleCallback, object userdata = null);
+        public abstract int[] LoadAssets(string[] rc, int[] priorities, LoadAssetCallback callback, LoadAssetCallback singleCallback, object userdata = null);
+        public abstract byte[] LoadTextBuffer(string mainPath, bool decode);
+        public abstract string LoadTextString(string mainPath, bool decode);
+
+        public abstract UnityEngine.Object LoadAssetImmediate(string rc);
+
+        public abstract void PrintBundleList();
+        public abstract void Release();
+        public abstract void Tick(uint uDeltaTimeMS);
+
+        public LoadAssetCallback ResourceCallBack { get; set; }
 
     }
 
