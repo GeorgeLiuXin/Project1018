fcbaacc54a43b731c25ce91cb57d8f0b569c5646
LiuXin <32125402+GeorgeLiuXin@users.noreply.github.com>
2019-01-24 22:08:26 +0800

    仇恨组件

--- Assets/Scripts/Actor/ActorObj.cs
+++ Assets/Scripts/Actor/ActorObj.cs
@@ -8,6 +8,7 @@ namespace XWorld
 	public class ActorObj : MonoBehaviour
 	{
         protected bool m_bFight;
+        public int m_nAvatarID;
 
 		// Use this for initialization
 		void Start()
@@ -34,6 +35,16 @@ namespace XWorld
         public int GetCamp()
         {
             return (int)CampType.Default;
+        }
+
+        public void SetAvatarID(int nAvatarID)
+        {
+            m_nAvatarID = nAvatarID;
+        }
+
+        public int GetAvatarID()
+        {
+            return m_nAvatarID;
         }
 
     }
--- Assets/Scripts/CombatSystem/Threat/ThreatComponent.cs
+++ Assets/Scripts/CombatSystem/Threat/ThreatComponent.cs
@@ -13,21 +13,17 @@ namespace XWorld
 
     public class ThreatComponent : ComponentBase
     {
-        protected bool m_bTauntState;
-        protected bool m_bNeedUpdate;
-
         protected int m_nTargetID;
         protected int m_nTargetThreat;
 
-        private List<int> m_RemoveList;
         protected Dictionary<int, Threat> m_ThreatDict;
+        private List<int> m_RemoveList;
 
         protected override void InitComponent()
         {
-            m_RemoveList = new List<int>();
             m_ThreatDict = new Dictionary<int, Threat>();
-            m_bTauntState = false;
-            m_bNeedUpdate = false;
+            m_RemoveList = new List<int>();
+            
             m_nTargetID = 0;
             m_nTargetThreat = 0;
             base.InitComponent();
@@ -35,18 +31,20 @@ namespace XWorld
 
         public override void OnPreDestroy()
         {
-            m_RemoveList.Clear();
             m_ThreatDict.Clear();
-            ClearThreat();
+            m_RemoveList.Clear();
+
+            m_nTargetID = 0;
+            m_nTargetThreat = 0;
             base.OnPreDestroy();
         }
 
         public void Update()
         {
-            //嘲讽状态
-            //TickTaunt(nFrameTime);
             //仇恨计时
             TickThreat(Time.deltaTime);
+            //刷新目标
+            TickTarget();
         }
 
         private void TickThreat(float fTime)
@@ -57,23 +55,12 @@ namespace XWorld
                 if (pThreat.fValidTime > 0)
                 {
                     pThreat.fValidTime -= fTime;
-                    if (pThreat.fValidTime<=0)
+                    if (pThreat.fValidTime <= 0)
                     {
-                        if (Owner)
-                        {
-                            GNodeAvatar* pAvatar = m_pOwnerNode->GetSceneAvatar(pThreat->m_nAvatarID);
-                            if (pAvatar)
-                            {
-                                pAvatar->RemoveThreat(m_pOwnerNode->GetAvatarID());
-                            }
-                        }
-
-                        if (pThreat->m_nAvatarID == m_nTargetID)
+                        if (pThreat.nAvatarID == m_nTargetID)
                         {
                             ResetTarget();
-                            m_bNeedUpdate = true;
                         }
-
                         m_RemoveList.Add(item.Key);
                         continue;
                     }
@@ -85,13 +72,6 @@ namespace XWorld
                 m_ThreatDict.Remove(key);
             }
             
-            //刷新仇恨目标
-            if (m_bNeedUpdate)
-            {
-                m_bNeedUpdate = false;
-                UpdateTarget();
-            }
-
             if (m_ThreatDict.Count == 0)
             {
                 if (Owner && Owner.IsFight())
@@ -101,25 +81,117 @@ namespace XWorld
             }
         }
 
-        private void TickTaunt(int nFrameTime)
+        public void TickTarget()
         {
-            if (m_bTauntState)
+            foreach (KeyValuePair<int, Threat> item in m_ThreatDict)
             {
-                if (Owner && !Owner->CheckState(GAS_Taunt))
+                Threat threat = item.Value;
+                if (threat.nThreat >= m_nTargetThreat * 1.1f)
                 {
-                    m_bTauntState = false;
-                    m_bNeedUpdate = true;
+                    SetTarget(threat.nAvatarID, threat.nThreat);
                 }
             }
-            else
+            //当前没有目标, 清空仇恨, 脱战
+            if (m_nTargetID == 0)
             {
-                if (m_pOwnerNode && m_pOwnerNode->CheckState(GAS_Taunt))
+                ResetTarget();
+            }
+        }
+
+        public void SetTarget(int nAvatarID, int nThreat)
+        {
+            m_nTargetID = nAvatarID;
+            m_nTargetThreat = nThreat;
+        }
+
+        public int GetTarget()
+        {
+            return m_nTargetID;
+        }
+
+        public int GetThreatByIndex(int id)
+        {
+            if (id < 0 || id >= m_ThreatDict.Count)
+            {
+                return 0;
+            }
+            int index = 0;
+            foreach (var item in m_ThreatDict)
+            {
+                if (index == id)
                 {
-                    m_bTauntState = true;
+                    return item.Key;
+                }
+                index++;
+            }
+            return 0;
+        }
+
+        public void ResetTarget()
+        {
+            m_nTargetID = 0;
+            m_nTargetThreat = 0;
+        }
+
+        public void AddThreat(ActorObj pAvatar)
+        {
+            if (Owner == null || pAvatar == null)
+                return;
+            if (Owner == pAvatar)
+                return;
+            
+            GThreat* pThreat = GetThreat(pAvatar->GetAvatarID(), true);
+            if (!pThreat)
+                return;
+
+            //当前有仇恨值(>=0)时,可以成为目标,仇恨值只能增加
+            if (nValue >= 0 || pThreat->m_nThreat <= 0)
+            {
+                pThreat->m_nThreat += nValue;
+            }
+            pThreat->m_nThreat = MAX(-1, pThreat->m_nThreat);
+            if (m_nTargetID == 0 || (!m_bTauntState && pThreat->m_nThreat >= m_nTargetThreat * 1.1f))
+            {
+                SetTarget(pThreat->m_nAvatarID, pThreat->m_nThreat);
+            }
+
+            //设置战斗状态
+            if (!m_pOwnerNode->IsFight())
+            {
+                m_pOwnerNode->EnterCombat();
+            }
+
+            //仇恨链接
+            if (bLink)
+            {
+                if (m_pOwnerNode->CheckRelation(pAvatar, ToEnemy))
+                {
+                    pAvatar->AddThreat(m_pOwnerNode, 0, false);
+                }
+                else
+                {
+                    pAvatar->AddThreat(m_pOwnerNode, -1, false);
                 }
             }
         }
 
+        public void RemoveThreat(int nAvatarID)
+        {
+            if (m_ThreatDict.ContainsKey(nAvatarID))
+            {
+                m_ThreatDict.Remove(nAvatarID);
+            }
+            if (m_nTargetID == nAvatarID)
+            {
+                ResetTarget();
+                TickTarget();
+            }
+        }
+
+        public void GetThreat()
+        {
+
+        }
 
         public void OnHurt()
         {
@@ -131,61 +203,16 @@ namespace XWorld
 
         }
 
-        public void AddThreat()
-        {
-
-        }
-
-        public void RemoveThreat()
-        {
-
-        }
-
-        public void ClearThreat()
-        {
-
-        }
-
-        public void GetThreat()
-        {
-
-        }
-        public void GetThreatByIndex(int id)
-        {
-
-        }
-
-        public int GetTarget()
-        {
-            return 0;
-        }
-
-        public void SetTarget()
-        {
-
-        }
-
-        public void ResetTarget()
-        {
-
-        }
-
-        public void UpdateTarget()
-        {
-
-        }
-
         public bool IsInThreatList(int nAvatarID)
         {
-            return false;
+            return m_ThreatDict.ContainsKey(nAvatarID);
         }
 
         public bool IsThreatListEmpty()
         {
-            return false;
+            return m_ThreatDict.Count == 0;
         }
-
-        public bool IsTaunt() { return m_bTauntState; } // 返回值本身就是由GAS_Taunt控制的，因此可提供直接使用的接口。
+        
         public int GetThreatCount()
         {
             return m_ThreatDict.Count;
@@ -247,95 +274,6 @@ namespace XWorld
 //            SetTarget(pAvatar->GetAvatarID(), pThreat->m_nThreat, true);
 //        }
 //    }
-//    //增加仇恨
-//    void GNodeThreatComponent::AddThreat(GNodeAvatar* pAvatar, int32 nValue, bool bLink)
-//    {
-//        if (m_pOwnerNode == NULL || pAvatar == NULL)
-//            return;
-
-//        if (!pAvatar->IsShow())
-//        {
-//            return;
-//        }
-
-//        if (m_pOwnerNode->GetAvatarID() == pAvatar->GetAvatarID())
-//            return;
-
-//        //不可选中的npc 不计算仇恨
-//        if (!pAvatar->IsCalculationThreat())
-//        {
-//            return;
-//        }
-
-//        GThreat* pThreat = GetThreat(pAvatar->GetAvatarID(), true);
-//        if (!pThreat)
-//            return;
-
-//        //当前有仇恨值(>=0)时,可以成为目标,仇恨值只能增加
-//        if (nValue >= 0 || pThreat->m_nThreat <= 0)
-//        {
-//            pThreat->m_nThreat += nValue;
-//        }
-//        pThreat->m_nThreat = MAX(-1, pThreat->m_nThreat);
-//        if (m_nTargetID == 0 || (!m_bTauntState && pThreat->m_nThreat >= m_nTargetThreat * 1.1f))
-//        {
-//            SetTarget(pThreat->m_nAvatarID, pThreat->m_nThreat);
-//        }
-
-//        //设置战斗状态
-//        if (!m_pOwnerNode->IsFight())
-//        {
-//            m_pOwnerNode->EnterCombat();
-//        }
-
-//        //仇恨链接
-//        if (bLink)
-//        {
-//            if (m_pOwnerNode->CheckRelation(pAvatar, ToEnemy))
-//            {
-//                pAvatar->AddThreat(m_pOwnerNode, 0, false);
-//            }
-//            else
-//            {
-//                pAvatar->AddThreat(m_pOwnerNode, -1, false);
-//            }
-//        }
-//    }
-//    //移除仇恨
-//    void GNodeThreatComponent::RemoveThreat(int32 nAvatarID)
-//    {
-//        if (m_vThreatList.Find(nAvatarID))
-//        {
-//            FACTORY_DELOBJ(m_vThreatList.Get());
-//            m_vThreatList.Remove();
-//        }
-//        if (m_nTargetID == nAvatarID)
-//        {
-//            ResetTarget();
-//            UpdateTarget();
-//        }
-//    }
-//    //清空仇恨
-//    void GNodeThreatComponent::ClearThreat()
-//    {
-//        //清空当前仇恨
-//        ResetTarget();
-//        //广播清除仇恨
-//        m_vThreatList.Begin();
-//        while (!m_vThreatList.IsEnd())
-//        {
-//            GThreat* pThreat = m_vThreatList.Get();
-//            if (pThreat)
-//            {
-//                GNodeAvatar* pAvatar = m_pOwnerNode->GetSceneAvatar(pThreat->m_nAvatarID);
-//                if (pAvatar)
-//                {
-//                    pAvatar->RemoveThreat(m_pOwnerNode->GetAvatarID());
-//                }
-//            }
-//            m_vThreatList.Remove();
-//        }
-//    }
 
 //    //获取仇恨
 //    GThreat* GNodeThreatComponent::GetThreat(int32 nAvatarID, bool bCreate)
@@ -356,69 +294,4 @@ namespace XWorld
 //            }
 //        }
 //        return pThreat;
-//    }
-
-//    bool GNodeThreatComponent::IsInThreatList(int32 nAvatarID)
-//    {
-//        return (m_vThreatList.Find(nAvatarID));
-//    }
-
-//    //获取仇恨对象
-//    int32 GNodeThreatComponent::GetTargetID()
-//    {
-//        return m_nTargetID;
-//    }
-
-//    void GNodeThreatComponent::SetTarget(int32 nAvatarID, int32 nThreat, bool bTaunt/* = false*/)
-//    {
-//        if (nThreat >= 0 || bTaunt)
-//        {
-//            if (m_pOwnerNode)
-//            {
-//                m_pOwnerNode->SetTargetID(nAvatarID);
-//            }
-//            m_nTargetID = nAvatarID;
-//            m_nTargetThreat = nThreat;
-//        }
-//    }
-
-//    void GNodeThreatComponent::ResetTarget()
-//    {
-//        m_nTargetID = 0;
-//        m_nTargetThreat = 0;
-//    }
-
-//    void GNodeThreatComponent::UpdateTarget()
-//    {
-//        m_vThreatList.Begin();
-//        while (!m_vThreatList.IsEnd())
-//        {
-//            GThreat* pThreat = m_vThreatList.Get();
-//            if (pThreat && pThreat->m_nThreat >= m_nTargetThreat * 1.1f)
-//            {
-//                SetTarget(pThreat->m_nAvatarID, pThreat->m_nThreat);
-//            }
-//            m_vThreatList.Next();
-//        }
-//        //当前没有目标, 清空仇恨, 脱战
-//        if (m_nTargetID == 0)
-//        {
-//            ClearThreat();
-//        }
-//    }
-
-//    bool GNodeThreatComponent::IsThreatListEmpty()
-//    {
-//        return m_vThreatList.empty();
-//    }
-
-//    Galaxy::GThreat* GNodeThreatComponent::GetThreatByIndex(int id)
-//    {
-//        if (id < 0 || id >= m_vThreatList.Count())
-//        {
-//            return NULL;
-//        }
-//        GThreatList::iterator iter = m_vThreatList.begin();
-//        std::advance(iter, id);
-//        return iter->second;
 //    }
